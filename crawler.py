f854ccb5cc54f13f705d2363356adf81d81b712d
Chiwan Song <chi3236@gmail.com>
2019-12-24 17:37:42 +0900

    making crawler

--- crawler.py
+++ crawler.py
@@ -1,6 +1,13 @@
 from bs4 import BeautifulSoup
-import urllib
+import requests
 
-response = urllib.urlopen("http://www.statiz.co.kr/stat.php")
-html = response.read()
-print "asdf"
+req = requests.get('http://www.statiz.co.kr/player.php')
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
+data = soup.find('div', {'class': 'wrapper'})
+data = data.find('div', {'class': 'content-wrapper'})
+data = data.find('div', {'class': 'container'})
+data = data.find('section', {'class': 'content'})
+data = data.find('div', {'class': 'row'})
+data = data.findAll('div', {'class': 'col-md-12 col-xs-12 col-sm-12 col-lg-12'})[1]
+data = data.find('div', {'class': 'row'})
